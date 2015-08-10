5a34782eba1a1a96088bb8f713e97f93c13c70c7
Roman Kuznietsov <roman.kuznietsov@gmail.com>
2015-08-10 15:09:45 +0300

    add redis client

--- mix.exs
+++ mix.exs
@@ -34,6 +34,7 @@ defmodule Spell.Mixfile do
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 1.4"},
      {:phoenix_live_reload, "~> 0.5", only: :dev},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:exredis, ">= 0.2.0"}]
   end
 end
